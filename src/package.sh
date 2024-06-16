2c090d7046712e5b6b6f379fc4a6303c9fa763a0
Przemysław Pieszyński <git@pieszynski.com>
2024-06-16 13:22:01 +0200

    update package to tag version

--- src/package.sh
+++ src/package.sh
@@ -7,3 +7,15 @@ cp package.json publish/
 cp dist/logpm.min.mjs publish/
 cp dist/logpm.mjs publish/
 cp dist/logpm.d.mts publish/
+
+if [[ ! -z "$PM_VERSION" ]]; then
+  SEM_VERSION=$(echo -n "$PM_VERSION" | sed -e 's/.*\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\).*/\1/')
+  echo "Setting version overwrite: '$SEM_VERSION'"
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' -e "s/\"version\": \"0.1.4\"/\"version\": \"$SEM_VERSION\"/g" publish/package.json
+  else
+    sed -i -e "s/\"version\": \"0.1.4\"/\"version\": \"$SEM_VERSION\"/g" publish/package.json
+  fi
+else
+  echo "No version overwrite, using file package.json as is"
+fi
