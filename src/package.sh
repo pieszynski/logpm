358be7496b177493e53c78fe85783d5a1604fde7
Przemysław Pieszyński <git@pieszynski.com>
2024-06-26 16:49:21 +0200

    Prepare version 1x

--- src/package.sh
+++ src/package.sh
@@ -12,9 +12,9 @@ if [[ ! -z "$PM_VERSION" ]]; then
   SEM_VERSION=$(echo -n "$PM_VERSION" | sed -e 's/.*\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\).*/\1/')
   echo "Setting version overwrite: '$SEM_VERSION'"
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' -e "s/\"version\": \"0.1.4\"/\"version\": \"$SEM_VERSION\"/g" publish/package.json
+    sed -i '' -e "s/\"version\": \"1.2.7\"/\"version\": \"$SEM_VERSION\"/g" publish/package.json
   else
-    sed -i -e "s/\"version\": \"0.1.4\"/\"version\": \"$SEM_VERSION\"/g" publish/package.json
+    sed -i -e "s/\"version\": \"1.2.7\"/\"version\": \"$SEM_VERSION\"/g" publish/package.json
   fi
 else
   echo "No version overwrite, using file package.json as is"
